407d8ba5c1732a6c13d15ee32879db10867a013a
Fabian Forero <nelsonfabian779@hotmail.com>
2014-05-27 17:28:48 -0500

    Resubida de repositorio

---
+++ frontend/lab-5/js/variables.js
@@ -0,0 +1,31 @@
+/* Variables */
+
+// comentario de una linea
+
+/* 
+  comentario multilines
+  comentario multilines
+*/
+
+/**
+ * esto es un comentario de documentacion
+ *exite valores del tpo @param parametro
+ *@return valor a devolver
+ */
+
+ var Uno, Dos;
+
+ Uno = 1;
+ Dos = "Dos";
+
+ console.log("El numero de la variable Uno es: "+Uno);
+ console.log("El Strign de la variable Dos es: "+Dos);
+
+console.log (typeof(Uno));
+console.log (typeof(Dos));
+
+// alert(Uno + Dos);
+
+//Mala practica
+otra = "Mala practica";
+console.log("El valor de la variable otro es: "+otra);
