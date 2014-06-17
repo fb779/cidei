97feb533bd9d63aee04c09ba3eace1be77a21aa6
Fabian Forero <nelsonfabian779@hotmail.com>
2014-06-16 21:02:47 -0500

    Ejercicio lab9 tareas con Grunt

---
+++ frontend/lab-9/lab9-1/Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		compass : {
+			dist: {
+				options : {
+					sassDir : 'dev/css/sass',
+					cssDir : 'dist/css',
+					environment : 'production'
+				}
+			},
+			dev : {
+				options : {
+					sassDir : 'dev/css/sass',
+					cssDir : 'dev/css/stylesheets'
+				
+				}	
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-compass');
+
+	grunt.registerTask('default', ['compass']);
+}
