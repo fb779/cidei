cdc3fe906e973ccb8c021c05cca236bded66750c
Fabian Forero <nelsonfabian779@hotmail.com>
2014-06-24 21:05:26 -0500

    Ejercicio lab1_9 clases en python

---
+++ backend/lab-1/lab1-1/lab1_9.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*- 
+"""
+ File: lab1_7.py
+ Resolver una funcio cuadrtica
+"""
+class AddressBook(object):
+	
+	def __init__(self):
+		"""constructor de la clase en Python"""
+		self._data_store = []
+
+	def add_contact(self, contact):
+		""" Adicionando contacto """
+		self._data_store.append(contact)
+
+	def all_contacts(self):
+		return	self._data_store
+
+	def print_contacts(self):
+		for contact in self._data_store:
+			print contact
+		
+class Contact(object):
+	def __init__(self, name, email='',phone=''):
+		"""constructor de la clase en Python"""
+		self.name = name
+		self.email = email
+		self.phone = phone
+
+	def __str__(self):
+		return "%s - %s - %s" % (self.name, self.email, self.phone)
+		
+
